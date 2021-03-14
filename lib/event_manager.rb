fc1844e7250347dbabdcac5659d5c30650feeae1
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-14 10:19:25 +0100

    Add erb file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -61,39 +61,53 @@
 
 # Iteration 3: Using Google’s Civic Information
 
-require 'csv'
-require 'google/apis/civicinfo_v2'
+# require 'csv'
+# require 'google/apis/civicinfo_v2'
 
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')[0..4]
-end
+# def clean_zipcode(zipcode)
+#   zipcode.to_s.rjust(5, '0')[0..4]
+# end
 
-def legislators_by_zipcode(zip)
-  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+# def legislators_by_zipcode(zip)
+#   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zip,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislators_names = legislators.map(&:name)
-    legislators_names.join(', ')
-  rescue
-    'You can find your representatives by visiting www....'
-  end
+#   begin
+#     legislators = civic_info.representative_info_by_address(
+#       address: zip,
+#       levels: 'country',
+#       roles: ['legislatorUpperBody', 'legislatorLowerBody']
+#     )
+#     legislators = legislators.officials
+#     legislators_names = legislators.map(&:name)
+#     legislators_names.join(', ')
+#   rescue
+#     'You can find your representatives by visiting www....'
+#   end
 
-end
+# end
 
-puts 'EventManager initialized.'
+# puts 'EventManager initialized.'
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
+# contents = CSV.open(
+#   'event_attendees.csv',
+#   headers: true,
+#   header_converters: :symbol
+# )
+
+# contents.each do |row|
+#   name = row[:first_name]
+
+#   zipcode = clean_zipcode(row[:zipcode])
+
+#   legislators = legislators_by_zipcode(zipcode)
+
+#   puts "#{name} #{zipcode} #{legislators}"
+# end
+
+# Iteration 4: Form Letters
+
+template_letter = File.read('form_letter.html')
 
 contents.each do |row|
   name = row[:first_name]
@@ -102,6 +116,9 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
 end
 
