1adb225f2cd1ec63b23c710e336e2f3018082516
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-14 10:36:14 +0100

    Change code to output to a file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -142,6 +142,7 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
@@ -150,6 +151,12 @@ contents.each do |row|
 
   form_letter = erb_template.result(binding)
 
-  puts form_letter
+  Dir.mkdir('output') unless Dir.exists?('output')
+    
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
 
