e855aa067861d4573c9243b864d05cc78658c1b6
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-14 11:05:29 +0100

    Clean and show the phone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -115,6 +115,23 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def clean_phone(phone)
+  phone = phone.gsub(/\D/, "")
+  if phone.length < 10
+    "Bad number - too short"
+  elsif phone.length == 10
+    phone
+  elsif phone.length == 11 
+    if phone[0] == '1'
+      phone[1..-1]
+    else
+      "Bad number, 11 but not 1"
+    end
+  else
+    "Bad number - too big"
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -156,6 +173,8 @@ contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
+
+  phone = clean_phone(row[:homephone])
 
   legislators = legislators_by_zipcode(zipcode)
 
